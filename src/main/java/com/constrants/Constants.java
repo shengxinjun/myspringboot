b37b5a840d0912e08dc6881e84dd6ad7f19c45db
498740190@qq.com <=498740190@qq.com>
2018-08-03 14:11:33 +0800

    常量类

---
+++ src/main/java/com/constrants/Constants.java
@@ -0,0 +1,17 @@
+package com.constrants;
+
+public interface Constants {
+	/**
+	 * 
+	 * 分页大小
+	 *
+	 */
+	interface pageSize{
+		
+		int SMALL_SIZE = 5;
+		
+		int MIDDLE_SIZE = 10;
+		
+		int LARGE_SIZE = 20;
+	}
+}
